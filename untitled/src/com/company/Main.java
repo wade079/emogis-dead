d651f545d8bb454281efa1b6533b4fade53de8d8
wade079 <99978276+wade079@users.noreply.github.com>
2022-03-31 16:13:35 +0200

    move
    move

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 import acm.graphics.*;
-import acm.program.*;
+import acm.program.GraphicsProgram;
 
 public class Main extends GraphicsProgram {
+
+    private String path ="C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";
+    private String [] names = {"caitlyn","derpsuo","jinx","kindred","lee_sin","lux","taric","teemo","vi","ziggs","zombi"};
 
     public static void main(String[] args) {
         new Main().start(args);
@@ -12,7 +15,15 @@ public class Main extends GraphicsProgram {
         int ancho = 80;
         int alto = 80;
 
-        setSize(900, 630);//tamaño del canvas
+        //Instanciamos los characteres
+        character[] emojis = new character[names.length];
+        for (int i = 0; i < emojis.length; i++) {
+            emojis[i]=new character(names[i]);
+            emojis[i].setPos();
+
+        }
+
+        setSize(900, 630);//tamaÃ±o del canvas
 
         GImage cielo = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\cielo.jpg" );
         cielo.setSize(400,73);
@@ -21,8 +32,6 @@ public class Main extends GraphicsProgram {
         GImage cielo_red = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\cielo_red.jpg" );
         cielo_red.setSize(400,73);
         add(cielo_red, 510,  0);
-
-
 
         GImage fondo = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\fondo_sanos.png");
         fondo.setSize(350,75);
@@ -33,12 +42,9 @@ public class Main extends GraphicsProgram {
         GImage vs = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\vs.png" );
         vs.setSize(200,80);
         add(vs, 350,  0);
-
         GImage terreno = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\terreno.jpg" );
         terreno.setSize(900,500);
         add(terreno, 0,  73);
-
-
 
 
         for (int i = 0; i < 900; i++) {
@@ -53,51 +59,23 @@ public class Main extends GraphicsProgram {
                 add(label, x, y);
             }
         }
-        GImage caitlyn = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\caitlyn.png");
-        caitlyn.setSize(alto,ancho);
-        add(caitlyn, 0,  85);
-
-        GImage derpsuo = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\derpsuo.png");
-        derpsuo.setSize(alto,ancho);
-        add(derpsuo, 80,  85);
-
-        GImage jinx = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\jinx.png");
-        jinx.setSize(alto,ancho);
-        add(jinx, 160,  85);
-
-        GImage kindred = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\kindred.png");
-        kindred.setSize(alto,ancho);
-        add(kindred, 230,  85);
-
-        GImage lee_sin = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\lee_sin.png");
-        lee_sin.setSize(alto,ancho);
-        add(lee_sin, 310,  85);
-
-        GImage lux = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\lux.png");
-        lux.setSize(alto,ancho);
-        add(lux, 390,  85);
-
-        GImage taric = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\taric.png");
-        taric.setSize(alto,ancho);
-        add(taric, 470,  85);
-
-        GImage teemo = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\teemo.png");
-        teemo.setSize(alto,ancho);
-        add(teemo, 550,  85);
-
-        GImage vi = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\vi.png");
-        vi.setSize(alto,ancho);
-        add(vi, 630,  85);
-
-        GImage ziggs = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\ziggs.png");
-        ziggs.setSize(alto,ancho);
-        add(ziggs, 700,  85);
-
-        GImage zombi = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\zombi.png");
-        zombi.setSize(alto,ancho);
-        add(zombi, 400,  480);
 
 
+
+        while (true){
+
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < emojis.length; i++) {
+
+                add(emojis[i].getImage(),emojis[i].getPos_x(),emojis[i].getPos_y());
+                emojis[i].move();
+            }
+
+        }
 
 
 
---
+++ untitled/src/com/company/character.java
@@ -0,0 +1,85 @@
+package com.company;
+import acm.graphics.*;
+/*Paul i Albert */
+
+public class character {
+
+
+    int min = -15;
+    int max = 15;
+
+    int size_x=80;
+    int size_y=80;
+
+    protected int pos_x;
+    protected int pos_y;
+
+    GImage image;
+
+    private boolean is_zombie;
+
+    public character(String name){
+        image = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\"+name+".png");
+        image.setSize(size_x,size_y);
+    }
+
+    public GImage getImage(){
+        return image;
+    }
+
+    public void move(){
+
+        int mover_x = getRandomNumber(min,max);
+        int mover_y = getRandomNumber(min,max);
+
+        this.pos_x=this.pos_x+mover_x;
+        this.pos_y=this.pos_y+mover_y;
+
+    }
+
+    public boolean getStatus(){
+        return is_zombie;
+    }
+
+    public void setPos(){
+        this.pos_x=getRandomNumber(0,850);
+        this.pos_y=getRandomNumber(80,580);//Por capcelera en canvas
+    }
+
+    public int getPos_x() {
+
+        if (pos_x <=0){
+            pos_x=0;
+        }
+        if (pos_x>=850){
+            pos_x=850;
+        }
+
+        return pos_x;
+    }
+    public int getPos_y(){
+
+        if (pos_y<=75){
+            pos_y=75;
+        }
+        if (pos_y>=520){
+            pos_y=520;
+        }
+
+        return pos_y;
+    }
+
+    public int getSize_x(){
+        return size_x;
+    }
+    public int getSize_y(){
+        return size_y;
+    }
+
+
+
+    public int getRandomNumber(int min, int max) {//generar numeros aleatorios
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+}
+
