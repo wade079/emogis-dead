1f544dcd61e872fdb4ffb77af1815ea758d9d055
wade079 <99978276+wade079@users.noreply.github.com>
2022-04-01 19:37:01 +0200

    modify
    modify terreno  animacion colicion

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -1,20 +1,18 @@
 package com.company;
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
+import java.awt.Color;
 
 public class Main extends GraphicsProgram {
 
-    private String path ="C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";
-    private String [] names = {"caitlyn","derpsuo","jinx","kindred","lee_sin","lux","taric","teemo","vi","ziggs","zombi"};
+    private final String [] names = {"caitlyn","derpsuo","jinx","kindred","lee_sin","lux","taric","teemo","vi","ziggs","zombi"};
 
     public static void main(String[] args) {
         new Main().start(args);
     }
 
     public void run() {
-        int ancho = 80;
-        int alto = 80;
-
+        setBackground(new Color(60,60,60));
         //Instanciamos los personajes
         character[] emojis = new character[names.length];
         for (int i = 0; i < emojis.length; i++) {
@@ -23,62 +21,122 @@ public class Main extends GraphicsProgram {
 
         }
 
-        setSize(900, 630);//tamaÃ±o del canvas
+        setSize(960, 680);//tamaÃ±o del canvas
 
-        GImage cielo = new GImage(path+"cielo.jpg" );
+        String path = "C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";
+        GImage cielo = new GImage(path +"cielo.jpg" );
         cielo.setSize(400,73);
         add(cielo, 0,  0);
 
-        GImage cielo_red = new GImage(path+"cielo_red.jpg" );
-        cielo_red.setSize(400,73);
-        add(cielo_red, 510,  0);
+        GImage cielo_red = new GImage(path +"cielo_red.jpg" );
+        cielo_red.setSize(350,73);
+        add(cielo_red, 605,  0);
 
-        GImage fondo = new GImage(path+"fondo_sanos.png");
+        GImage fondo = new GImage(path +"fondo_sanos.png");
         fondo.setSize(350,75);
         add(fondo, 0,  0);
-        GImage fondo1 = new GImage(path+"fondo.png");
+        GImage fondo1 = new GImage(path +"fondo.png");
         fondo1.setSize(350,75);
-        add(fondo1, 550,  0);
-        GImage vs = new GImage(path+"vs.png" );
-        vs.setSize(200,80);
-        add(vs, 350,  0);
-        GImage terreno = new GImage(path+"terreno.jpg" );
-        terreno.setSize(900,500);
-        add(terreno, 0,  73);
+        add(fondo1, 595,  0);
+        GImage vs = new GImage(path +"vs.gif" );
+        vs.setSize(260,80);
+        add(vs, 375,  0);
+        GImage terreno = new GImage(path +"terreno.jpg" );
+        terreno.setSize(950,550);
+        add(terreno, 0,  70);
 
-        for (int i = 0; i < 900; i++) {
-            for (int j = 0; j < 1; j++) {
+        for (int i = 0; i < 50; i++) {
+            GImage fuego_isq = new GImage(path + "pinchos.gif");
+            GImage fuego_der = new GImage(path + "pinchos.gif");
+            GImage fuego_arr = new GImage(path + "pinchos.gif");
+            GImage fuego_ava = new GImage(path + "pinchos.gif");
+            int fuego_ancho = 180;
+            int fuego_alto = 50;
 
-                GLabel label = new GLabel("-");
-                label.setFont("SansSerif-30");
-                double x = (getWidth());
-                double y = (getHeight());
-                x = x + i;
-                y = y + j * 80;
-                add(label, x, y);
-            }
+            int y = 0;
+            y = y + i * 40;
+          add(fuego_isq, 840, y + 55);
+            fuego_isq.setSize(fuego_ancho, fuego_alto);
+           add(fuego_der, -80, y + 55);
+            fuego_der.setSize(fuego_ancho, fuego_alto);
+            add(fuego_arr, y +-840,  55);
+            fuego_arr.setSize(fuego_ancho, fuego_alto);
+          add( fuego_ava, y +-80,  574);
+            fuego_ava.setSize(fuego_ancho, fuego_alto);
         }
 
-        while (true){
+
+
+        /*
+        contador de caracter buenos
+         */
+
+        int cont_zombie=1;
+
+        int ciclo = 0;
+        while (cont_zombie<= emojis.length-1) {
 
             try {
-                Thread.sleep(150);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             for (int i = 0; i < emojis.length; i++) {
 
-                add(emojis[i].getImage(),emojis[i].getPos_x(),emojis[i].getPos_y());
+                add(emojis[i].getImage(), emojis[i].getPos_x(), emojis[i].getPos_y());
+
+            }
+            for (int i = 0; i < emojis.length; i++) {
                 emojis[i].move();
             }
 
-        }
+            if (ciclo == 5) {
+                for (int j = 0; j < emojis.length - 1; j++) {
+                    for (int k = j + 1; k < emojis.length; k++) {
 
+                        if ((emojis[j].getPos_x() >= emojis[k].getPos_x()) && (emojis[j].getPos_x() <= emojis[k].getPos_x() + 80)
+                                &&
+                                (emojis[j].getPos_y() >= emojis[k].getPos_y()) && (emojis[j].getPos_y() <= emojis[k].getPos_y() + 80)) {
+
+
+                            if (emojis[j].getStatus() && emojis[k].getStatus()) {
+                                emojis[k].check_collision();
+
+                            }
+
+                            if (!emojis[j].getStatus() && !emojis[k].getStatus()) {
+                                emojis[k].check_collision();
+                            }
+
+                            if (emojis[j].getStatus() && !emojis[k].getStatus()) {
+                                emojis[k].setStatus(true);
+                                emojis[k].setZombie();
+                                emojis[k].check_collision();
+                                cont_zombie++;
+
+                            }
+                            if (!emojis[j].getStatus() && emojis[k].getStatus()) {
+                                emojis[j].setStatus(true);
+                                emojis[j].setZombie();
+                                emojis[k].check_collision();
+                                cont_zombie++;
+                            }
+                        }
+                    }
+                    ciclo = 0;
+                }
+            }
+            ciclo++;
+        }
     }
+
     public int getHeight() {
-        return 80;
+        return 105;
     }
     public int getWidth() {
         return 0;
     }
+
+
+
 }
--- untitled/src/com/company/character.java
+++ untitled/src/com/company/character.java
@@ -1,85 +1,164 @@
 package com.company;
+
+
 import acm.graphics.*;
 /*Paul i Albert */
 
 public class character {
 
 
-    int min = -15;
-    int max = 15;
+    int mover_x = 1;
+    int mover_y = -1;
 
-    int size_x=80;
-    int size_y=80;
+    int size_x = 80;
+    int size_y = 80;
 
     protected int pos_x;
     protected int pos_y;
 
     GImage image;
 
-    private boolean is_zombie;
+    protected boolean is_zombie=false;
 
     public character(String name){
-        image = new GImage("img\\"+name+".png");
-        image.setSize(size_x,size_y);
+        image = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\"+name+".png");
+
+        if (name.equals("zombi")){
+            is_zombie=true;
+
+        }
+
+        if (is_zombie) {
+            image = new GImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\zombi.png");
+
+        }
+        image.setSize(size_x, size_y);
     }
 
-    public GImage getImage(){
+    public GImage getImage() {
         return image;
     }
 
-    public void move(){
+    public void setZombie(){
 
-        int mover_x = getRandomNumber(min,max);
-        int mover_y = getRandomNumber(min,max);
+        image.setImage("C:\\Users\\wade079\\Desktop\\emogisdead\\img\\zombi.png");
+        image.setSize(size_x, size_y);
+    }
 
-        this.pos_x=this.pos_x+mover_x;
-        this.pos_y=this.pos_y+mover_y;
+    public void move() {
+
+        /*int mover_x = getRandomNumber(min,max);
+        int mover_y = getRandomNumber(min,max);*/
+
+
+        this.pos_x = this.pos_x + mover_x;
+        this.pos_y = this.pos_y + mover_y;
+
+        check_wall(this.pos_x, this.pos_y);
+
 
     }
 
-    public boolean getStatus(){
+    public boolean getStatus() {
         return is_zombie;
     }
+    public void setStatus(boolean zombie){
+        this.is_zombie=true;
+    }
 
-    public void setPos(){
-        this.pos_x=getRandomNumber(0,850);
-        this.pos_y=getRandomNumber(80,580);//Por capcelera en canvas
+    public void setPos() {
+        this.pos_x = getRandomNumber(0, 930);
+        this.pos_y = getRandomNumber(80, 580);//Por capcelera en canvas
     }
 
     public int getPos_x() {
 
-        if (pos_x <=0){
-            pos_x=0;
+        if (pos_x <= 0) {
+            pos_x = 0;
         }
-        if (pos_x>=850){
-            pos_x=850;
+        if (pos_x >= 930 - size_x) {
+            pos_x =930 - size_x;
         }
 
         return pos_x;
     }
-    public int getPos_y(){
 
-        if (pos_y<=75){
-            pos_y=75;
+    public int getPos_y() {
+
+        if (pos_y <= 90) {
+            pos_y = 90;
         }
-        if (pos_y>=520){
-            pos_y=520;
+        if (pos_y >= 600 - size_y) {
+            pos_y = 600 - size_y;
         }
 
         return pos_y;
     }
 
-    public int getSize_x(){
+    public int getSize_x() {
         return size_x;
     }
-    public int getSize_y(){
+
+    public int getSize_y() {
         return size_y;
+    }
+
+    public void check_wall(int pos_x, int pos_y) {
+
+        if (pos_x <= 0) {
+            this.mover_x = 1;
+        }
+        if (pos_x >= 930 - size_x) {
+            this.mover_x = -1;
+        }
+
+        if (pos_y <= 90) {
+            this.mover_y = 1;
+        }
+        if (pos_y >= 600 - size_y) {
+            this.mover_y = -1;
+        }
+
+    }
+
+    public void check_collision(){
+
+        if (mover_x==1){
+            mover_x=-1;
+        }else if (mover_x==-1){
+            mover_x=1;
+        }
+
+        if (mover_y==1){
+            mover_y=-1;
+        }else if (mover_y==-1){
+            mover_y=1;
+        }
+
+
+
     }
 
 
 
     public int getRandomNumber(int min, int max) {//generar numeros aleatorios
         return (int) ((Math.random() * (max - min)) + min);
+    }
+}
+class zombie extends character{
+
+    int pos_x=super.pos_x;
+    int pos_y=super.pos_y;
+
+
+
+    public zombie(String name) {
+        super(name);
+    }
+
+    public void setPos() {
+        this.pos_x = getRandomNumber(0,930);
+        this.pos_y = getRandomNumber(80, 580);//Por capcelera en canvas
     }
 }
 
