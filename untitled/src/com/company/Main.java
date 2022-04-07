4c69091b577d33c507d0cae6926008cca044840b
wade079 <99978276+wade079@users.noreply.github.com>
2022-04-07 15:46:36 +0200

    COMENT
    COMENT

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -6,16 +6,18 @@ import acm.util.SoundClip;
 
 public class Main extends GraphicsProgram {
 
-    private final String[] names = {"caitlyn", "derpsuo", "jinx", "kindred", "lee_sin", "lux", "taric", "teemo", "vi", "ziggs", "zombi"};
-    private final String path = "C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";
-    private final String path_music = "C:\\Users\\wade079\\Desktop\\emogisdead\\music\\";
+    private final String[] names = {"caitlyn", "derpsuo", "jinx", "kindred", "lee_sin", "lux", "taric", "teemo", "vi", "ziggs", "zombi"};//Lista de personages presentes en la pantalla
+    private final String path_image = "C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";//Directorio de las imagenes
+    private final String path_music = "C:\\Users\\wade079\\Desktop\\emogisdead\\music\\";//Directorio de las pistas de audio
 
 
     public static void main(String[] args) {//main
         new Main().start(args);
     }
 
-    public void run() {//metode run
+    public void run() {//Donde se arranca el juego e implementamos sonido, imagenes con Acm Graphics
+
+        //metodos usados
         canvas();
         ini();
         maping();
@@ -31,7 +33,7 @@ public class Main extends GraphicsProgram {
     }
 
     private void ini() {//metode "ini" inici de partida
-        GImage game_ini = new GImage(path + "ini.gif");//definim la ruta i el nom del gif
+        GImage game_ini = new GImage(path_image + "ini.gif");//definim la ruta i el nom del gif
         game_ini.setSize(960, 680);//definim mida del gif
         add(game_ini, 0, 0);//definim posicio gif
 
@@ -39,7 +41,7 @@ public class Main extends GraphicsProgram {
         start.setVolume(1);//volum audio inici
         start.play();//play a l'audio
         try {
-            Thread.sleep(14500);
+            Thread.sleep(14500);//Sleep entre la transicion de la pantalla de carga y el inicio del juego
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -49,36 +51,48 @@ public class Main extends GraphicsProgram {
 
     }
 
-    private void maping() {//metode per grafics de la partida
-        GImage cielo = new GImage(path + "cielo.jpg");//imatge
+    private void maping() {
+
+        //metode per grafics de la partida
+        GImage cielo = new GImage(path_image + "cielo.jpg");//imagen del cielo azul
         cielo.setSize(400, 73);//mida imatge
         add(cielo, 0, 0);//posicio imatge
 
-        GImage cielo_red = new GImage(path + "cielo_red.jpg");
+        //Imagen del cielo rojo (zombis)
+        GImage cielo_red = new GImage(path_image + "cielo_red.jpg");
         cielo_red.setSize(350, 73);
         add(cielo_red, 605, 0);
 
-        GImage fondo = new GImage(path + "fondo_sanos.png");
+        //Fondo al lado del contador de los sanos
+        GImage fondo = new GImage(path_image + "fondo_sanos.png");
         fondo.setSize(350, 75);
         add(fondo, 0, 0);
-        GImage fondo1 = new GImage(path + "fondo.png");
+
+        //Fondo al lado del contador de zombis
+        GImage fondo1 = new GImage(path_image + "fondo.png");
         fondo1.setSize(350, 75);
         add(fondo1, 595, 0);
-        GImage vs = new GImage(path + "vs.gif");
+
+        //Imagen del "versus" animado
+        GImage vs = new GImage(path_image + "vs.gif");
         vs.setSize(70, 70);
         add(vs, 466, 0);
-        GImage terreno = new GImage(path + "terreno.jpg");
+
+        //Imagen de fondo por donde se desplazan los personajes
+        GImage terreno = new GImage(path_image + "terreno.jpg");
         terreno.setSize(950, 550);
         add(terreno, 0, 70);
 
     }
 
-    private void lanzallamas() {//metode per limits del mapa
-        for (int i = 0; i < 43; i++) {//bucle per rodejar el mapa
-            GImage fuego_izq = new GImage(path + "fuego.gif");//ruta+nom imatge
-            GImage fuego_der = new GImage(path + "fuego.gif");
-            GImage fuego_arr = new GImage(path + "fuego.gif");
-            GImage fuego_aba = new GImage(path + "fuego.gif");
+    private void lanzallamas() {
+
+        //metode per limits del mapa
+        for (int i = 0; i < 43; i++) {//bucle per rodejar el mapa de flames
+            GImage fuego_izq = new GImage(path_image + "fuego.gif");//ruta+nom imatge
+            GImage fuego_der = new GImage(path_image + "fuego.gif");
+            GImage fuego_arr = new GImage(path_image + "fuego.gif");
+            GImage fuego_aba = new GImage(path_image + "fuego.gif");
             int fuego_ancho = 180;//amplada del mapa
             int fuego_alto = 50;//altura del mapa
 
@@ -113,16 +127,18 @@ public class Main extends GraphicsProgram {
             emojis[i] = new character(names[i]);
             emojis[i].setPos();//apareixen a la posicio aleatoria del metode setPos
         }
-        int cont_zombie = 1;//variables
-        int ciclo = 0;
-        int contador_sanos;
-        int contador_zombi;
+        int cont_zombie = 1;//contador de zombis inicializado a 1 porque empezamos con 1 zombi
+        int ciclo = 0;//contador de ciclos para eficiencia del programa
+        int contador_sanos;//contador de personajes sanos
 
-        contador_sanos = names.length - cont_zombie;
-        GLabel sanos = new GLabel(toString(contador_sanos));//comptador per persones sanes
+        int contador_zombi;//el adri lo explicara
+
+        contador_sanos = names.length - cont_zombie;//inicializamos el contador de sanos
+
+        GLabel sanos = new GLabel(toString(contador_sanos));//mostrem comptador per persones sanes
 
         contador_zombi = cont_zombie;
-        GLabel zombi = new GLabel(toString(contador_zombi));//contador per zombies
+        GLabel zombi = new GLabel(toString(contador_zombi));//mostrem contador per zombies
 
         while (cont_zombie <= emojis.length - 1) {// mentres hi hagi persones sanes
 
@@ -131,57 +147,82 @@ public class Main extends GraphicsProgram {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            //actualizamos las posiciones de todos los personajes en la pantalla eso genera el efecto de movimiento
             for (character emoji : emojis) {
+
                 add(emoji.getImage(), emoji.getPos_x(), emoji.getPos_y());
                 emoji.move();
             }
 
+            //cada 5 ciclos verificamos colisiones
             if (ciclo >= 5) {
+                //Se comparan todos los personajes para ver si alguno toca con otro
                 for (int j = 0; j < emojis.length - 1; j++) {
                     for (int k = j + 1; k < emojis.length; k++) {
 
+                        //Verificamos si colisiona sobre un margen de coordenadas x e Y ej. x hasta x+80 (80 que es la anchura de la imagen)
                         if ((emojis[j].getPos_x() >= emojis[k].getPos_x()) && (emojis[j].getPos_x() <= emojis[k].getPos_x() + 80)
                                 &&
                                 (emojis[j].getPos_y() >= emojis[k].getPos_y()) && (emojis[j].getPos_y() <= emojis[k].getPos_y() + 80)) {
+
+                            //Si hay colision comparamos quien ha tocado
+
+                            //Si son 2 sanos
                             if (emojis[j].getStatus() && emojis[k].getStatus()) {
-                                emojis[k].check_collision();
+                                emojis[k].check_collision();//Hace cambiar la direccion de movimiento
                             }
+
+                            //Si son 2 zombis
                             if (!emojis[j].getStatus() && !emojis[k].getStatus()) {
-                                emojis[k].check_collision();
+                                emojis[k].check_collision();//Hace cambiar la direccion de movimiento
                             }
+
+                            //Si es un sano y un zombi
                             if (emojis[j].getStatus() && !emojis[k].getStatus()) {
-                                emojis[k].setStatus();
-                                emojis[k].setZombie();
-                                emojis[k].check_collision();
-                                cont_zombie++;
-                                contador_sanos--;
-                                contador_zombi++;
-                                damage();
+                                emojis[k].setStatus();//El personaje sano se convierte en zombi
+                                emojis[k].setZombie();//Actualizamos la imagen por la del zombi
+                                emojis[k].check_collision();//Cambiamos direccion de movimiento
+
+                                cont_zombie++;//incrementamos zombis
+                                contador_sanos--;//quitamos un sano
+                                contador_zombi++;//Eso es del Adri
+
+                                damage();//Sonido de cuando uno se convierte en zombi
                             }
+
+                            //Si es un zombi y un sano
                             if (!emojis[j].getStatus() && emojis[k].getStatus()) {
-                                emojis[j].setStatus();
-                                emojis[j].setZombie();
-                                emojis[k].check_collision();
-                                cont_zombie++;
-                                contador_zombi++;
-                                contador_sanos--;
-                                damage();
+                                emojis[j].setStatus();//El personaje sano se convierte en zombi
+                                emojis[j].setZombie();//Actualizamos la imagen por la del zombi
+                                emojis[k].check_collision();//Cambiamos direccion de movimiento
+                                cont_zombie++;//incrementamos zombis
+                                contador_zombi++;//quitamos un sano
+                                contador_sanos--;//Eso es del Adri
+
+                                damage();//Sonido de cuando uno se convierte en zombi
                             }
                         }
                     }
-                    ciclo = 0;
+                    ciclo = 0;//Reseteamos los ciclos
                 }
             }
-            ciclo++;
+            ciclo++;//Incrementamos un ciclo
+
+            //Contador de sanos
             sanos.setLabel(toString(contador_sanos));
             sanos.setFont("SansSerif-50");
             add(sanos, 410, 55);//afegim el contador a la posicio
 
+
+            //Contador de infectados
             zombi.setLabel(toString(contador_zombi));
             zombi.setFont("SansSerif-50");
             add(zombi, 550, 55);
 
-        }
+        }//Final del while
+
+
+        //Paramos el juego durante 1500 millis
         try {
             Thread.sleep(1500);
         } catch (InterruptedException e) {
@@ -198,14 +239,14 @@ public class Main extends GraphicsProgram {
 
     private void fin_partida() {//fi de la partida
 
-        SoundClip fin = new SoundClip(path_music + "final.wav");
-        SoundClip game_over = new SoundClip(path_music + "game_over.wav");
+        SoundClip fin = new SoundClip(path_music + "final.wav");//Sonido de fin
+        SoundClip game_over = new SoundClip(path_music + "game_over.wav");//Sonido game over
         fin.setVolume(0.5);
         game_over.setVolume(0.5);
 
         fin.play();
         game_over.play();
-        GImage game_overi = new GImage(path + "game_over.gif");
+        GImage game_overi = new GImage(path_image + "game_over.gif");//Mostramos la pantalla final
         game_overi.setSize(960, 680);//
         add(game_overi, 0, 0);
     }
--- untitled/src/com/company/character.java
+++ untitled/src/com/company/character.java
@@ -6,73 +6,77 @@ import acm.graphics.*;
 public class character {
 
 
-    int mover_x = 1;
-    int mover_y = -1;
+    int mover_x = 1;//Coordenadas de movimiento x
+    int mover_y = -1;//Coordenadas de movimiento Y
 
-    int size_x = 80;
-    int size_y = 80;
+    int size_x = 80;//Tamaño del personaje
+    int size_y = 80;//
 
+    //Posicion inicial no definida
     protected int pos_x;
     protected int pos_y;
-    public final String path = "C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";
+
+    private final String path = "C:\\Users\\wade079\\Desktop\\emogisdead\\img\\";
     GImage image;
 
     protected boolean is_zombie=false;
 
+    //metodo constructor de personaje
     public character(String name){
-        image = new GImage(path+name+".png");
+        image = new GImage(path+name+".png");//Atribuimos una imagen al personaje
 
-        if (name.equals("zombi")){
+        if (name.equals("zombi")){//Si el nombre es zombi actualizamos el status del personaje a zombi
             is_zombie=true;
 
         }
 
-        if (is_zombie) {
+        if (is_zombie) {//Si es un zombi le ponemos la imagen de zombi
             image = new GImage(path+"zombi.gif");
 
         }
-        image.setSize(size_x, size_y);
+        image.setSize(size_x, size_y);//Atribuimos el tamaño de la imagen
     }
 
     public GImage getImage() {
         return image;
-    }
+    }//Retorna la imagen del personage
 
-    public void setZombie(){
+    public void setZombie(){//Metodo para convertir en zombi
 
         image.setImage(path+"zombi.gif");
         image.setSize(size_x, size_y);
     }
 
-    public void move() {
+    public void move() {//Metodo de mover el caracter
 
 
         this.pos_x = this.pos_x + mover_x;
         this.pos_y = this.pos_y + mover_y;
 
-        check_wall(this.pos_x, this.pos_y);
+        check_wall(this.pos_x, this.pos_y);//Metodo para comprovar si hay colision con el borde del juego
 
 
     }
 
     public boolean getStatus() {
         return is_zombie;
-    }
-    public void setStatus(){
+    }//Metodo que nos devuelve el estado del personaje
+
+    public void setStatus(){//Para convertir el personaje en zombi
         this.is_zombie=true;
     }
 
-    public void setPos() {
+    public void setPos() {//Metodo para atribuir la posicion de forma aleatoria a los personajes en el area de juego
         this.pos_x = getRandomNumber(0, 930);
         this.pos_y = getRandomNumber(80, 580);//Por capcelera en canvas
     }
 
-    public int getPos_x() {
+    public int getPos_x() {//Nos retorna la posicion actual del personaje
 
-        if (pos_x <= 0) {
+        if (pos_x <= 0) {//Comprobacion de choque contra pared izquierda y que no pase de alli
             pos_x = 0;
         }
-        if (pos_x >= 930 - size_x) {
+        if (pos_x >= 930 - size_x) {//Comprobacion de choque de pared derecho y que no pase de alli
             pos_x =930 - size_x;
         }
 
@@ -81,35 +85,35 @@ public class character {
 
     public int getPos_y() {
 
-        if (pos_y <= 90) {
+        if (pos_y <= 90) {//Comprobacion de choque de pared superior y que no pase de alli
             pos_y = 90;
         }
-        if (pos_y >= 600 - size_y) {
+        if (pos_y >= 600 - size_y) {//Comprobacion de choque de pared y que no pase de alli
             pos_y = 600 - size_y;
         }
 
         return pos_y;
     }
 
-    public void check_wall(int pos_x, int pos_y) {
+    public void check_wall(int pos_x, int pos_y) {//Metodo para cambiar la direccion de movimiento del personaje
 
-        if (pos_x <= 0) {
-            this.mover_x = 1;
+        if (pos_x <= 0) {//Si choca hacia la izquierda
+            this.mover_x = 1;//Que valla hacia la derecha
         }
-        if (pos_x >= 930 - size_x) {
-            this.mover_x = -1;
+        if (pos_x >= 930 - size_x) {//Si choca a la derecha
+            this.mover_x = -1;//Que vaya a la izquierda
         }
 
-        if (pos_y <= 90) {
-            this.mover_y = 1;
+        if (pos_y <= 90) {//Si choca arriba
+            this.mover_y = 1;//Que vaya a bajo
         }
-        if (pos_y >= 600 - size_y) {
-            this.mover_y = -1;
+        if (pos_y >= 600 - size_y) {//Si choca a bajo
+            this.mover_y = -1;//Que vaya arriba
         }
 
     }
 
-    public void check_collision(){
+    public void check_collision(){//Metodo para cambiar de direccion al personaje cuando choca con otro
 
         if (mover_x==1){
             mover_x=-1;
@@ -129,6 +133,6 @@ public class character {
 
     public int getRandomNumber(int min, int max) {//generar numeros aleatorios
         return (int) ((Math.random() * (max - min)) + min);
-    }
+    }//Metodo de aleatoriedad
 }
 
